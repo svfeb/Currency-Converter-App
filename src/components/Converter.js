a6f170ec0359ab0f701600a7bf9bbcc08eb3e05e
shubham verma <sv.feb8@gmail.com>
2022-08-24 19:49:34 +0530

    updated

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,17 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HiSwitchHorizontal } from "react-icons/hi";
+import Data from "./Data";
 
 function Converter() {
-  const [from, setFrom] = useState("USD");
-  const [to, setTo] = useState("EUR");
-  const [input, setInput] = useState(0);
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [input, setInput] = useState("");
+  const [inputCurrency, setInputCurrency] = useState(1);
+  const [targetCurrency, setTargetCurrency] = useState(0);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+  const [exchangeRate, setExchangeRate] = useState();
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = input;
+    toAmount = input * exchangeRate;
+  } else {
+    toAmount = input;
+    fromAmount = input / exchangeRate;
+  }
+
+  useEffect(() => {
+    const firstCurrency = Data.map((inner) => inner.exchangeRate);
+    setCurrencyOptions(Data.map((inner) => inner.exchangeRate));
+    setFrom(Data.base);
+    setTo(firstCurrency);
+    setExchangeRate([firstCurrency]);
+  }, []);
+
+  // function convert(){
+  //   setTargetCurrency(()=>{
+  //     if()
+  //   })
+  // }
+  // useEffect(() => {
+  //   Data.forEach((o) => {
+  //     if (o.currency === to) {
+  //       console.log(o);
+  //     }
+  //   });
+  // });
+  // const currency = Data.map((key) => {
+  //   return key.exchangeRate;
+  // });
+  // console.log(currency);
+
+  // const [options, setOptions] = useState([]);
+  // const [output, setOutput] = useState(0);
 
   const change = (e) => {
     setInput(e.target.value);
   };
 
   function flip() {
-    var temp = from;
+    const temp = from;
     setFrom(to);
     setTo(temp);
   }
@@ -21,16 +64,29 @@ function Converter() {
       <label htmlFor="amount">Amount: </label>
 
       <input
+        value={input}
         type="number"
         name="amount"
         style={{ margin: "30px" }}
         onChange={change}
+        placeholder="0"
         required
       />
 
       <label htmlFor="From">From: </label>
-
-      <input type="number" name="From" style={{ margin: "30px" }} required />
+      <select
+        name="From"
+        style={{ margin: "30px" }}
+        onChange={(e) => {
+          setFrom(e.target.value);
+        }}
+        value={from}
+        placeholder="Select From"
+      >
+        {Data.map((acc) => (
+          <option value={acc.currency}>{acc.currency}</option>
+        ))}
+      </select>
 
       <HiSwitchHorizontal
         size="20px"
@@ -39,10 +95,21 @@ function Converter() {
         }}
         style={{ margin: "30px", cursor: "pointer" }}
       />
-
       <label htmlFor="To">To: </label>
 
-      <input type="number" name="To" style={{ margin: "30px" }} required />
+      <select
+        name="To"
+        style={{ margin: "30px" }}
+        onChange={(e) => {
+          setTo(e.target.value);
+        }}
+        value={to}
+        placeholder="Select From"
+      >
+        {Data.map((acc) => (
+          <option value={acc.currency}>{acc.currency}</option>
+        ))}
+      </select>
 
       <button className="btn btn-primary my-3" style={{ margin: "30px" }}>
         Convert
